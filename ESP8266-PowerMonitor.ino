3710bf9b52bf298f6a56f261d6cb62bfce5284cc
Jan Szumiec <jan.szumiec@gmail.com>
2015-12-01 23:40:21 +0000

    I need to actually hook this up to do any testing.

--- ESP8266-PowerMonitor.ino
+++ ESP8266-PowerMonitor.ino
@@ -3,12 +3,36 @@
 #include <WiFiClientSecure.h>
 #include "settings.h"
 
-void setup() {
-  // put your setup code here, to run once:
+int counter;
 
+void setup() {
+  Serial.begin(115200);
+  Serial.println();
+  Serial.print("Connecting to network: ");
+  Serial.println(WIFI_SSID);
+
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+
+  Serial.println(WiFi.localIP());
+
+  // When there's a pulse, increment the counter.
+  pinMode(16, INPUT_PULLUP);
+  attachInterrupt(16, pulse_ISR, RISING);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  noInterrupts();
 
+  counter = 0;
+  interrupts();
+  delay(5 * 60 * 1000); // every 5 minutes
 }
+
+void pulse_ISR(void) {
+  counter++;
+}
+
