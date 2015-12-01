b15ce1411eb5b664d2a8f2c9f2e1acd6c899de30
Jan Szumiec <jan.szumiec@gmail.com>
2015-12-01 20:04:44 +0000

    Initial commit.

---
+++ ESP8266-PowerMonitor.ino
@@ -0,0 +1,14 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266WiFiMulti.h>
+#include <WiFiClientSecure.h>
+#include "settings.h"
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
