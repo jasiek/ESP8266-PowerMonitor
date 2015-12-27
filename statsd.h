e20986536d7adef325957660f384e9c648eef0b8
Jan Szumiec <jan.szumiec@gmail.com>
2015-12-27 09:18:17 +0100

    statsd class shamelessly ripped off from https://github.com/zackkitzmiller/statsd-arduino

---
+++ statsd.h
@@ -0,0 +1,24 @@
+#ifndef Statsd_h
+#define Statsd_h
+
+#include "Arduino.h"
+#include "IPAddress.h"
+#include "WiFi.h"
+#include "WiFiUdp.h"
+
+class StatsD
+{
+public:
+  StatsD(IPAddress ip, int port, int localPort);
+  void increment(const char *metric);
+  void decrement(const char *metric);
+  void timing(const char *metric, int ms);
+  void gauge(const char *metric, int gaugeValue);
+  void sets(const char *metric, int setsValue);
+private:
+  void _send(const char *metric, int value, const char *cmd);
+  IPAddress _ip;
+  int _port;
+};
+
+#endif
