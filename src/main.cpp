d183c553584f0c06ca2f00f6910f4a6071d22304
Jan Szumiec <jan.szumiec@gmail.com>
2017-02-06 21:16:14 +0100

    Quick hack to enable remote update shim and basic functionality.

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,8 @@
 #include <Arduino.h>
-
-#include <ArduinoJson.h>
 #include <DallasTemperature.h>
 #include <OneWire.h>
-#include <ESP8266WiFi.h>
-#include <ESP8266WiFiMulti.h>
-#include <ESP8266mDNS.h>
-#include <PubSubClient.h>
 #include <FS.h>
+#include <metering.h>
 #include "IPAddress.h"
 #include "string.h"
 
@@ -22,9 +17,6 @@ ADC_MODE(ADC_VCC);
 
 OneWire bus(ONEWIRE_PIN); // Use GPIO4 as it is not connected to any peripherals
 DallasTemperature sensors(&bus);
-ESP8266WiFiMulti WiFiMulti;
-WiFiClient client;
-PubSubClient mqtt(client);
 
 String nodeName;
 int movementCounter;
@@ -35,12 +27,8 @@ unsigned long pulseWidth;
 unsigned long packetsSent;
 float voltage;
 float temperature;
-int rssi;
 
-String WIFI_SSID;
-String WIFI_PASS;
-String MQTT_SERVER;
-int MQTT_PORT;
+
 int METER_PULSE_WIDTH;
 
 void pulseStart();
@@ -51,51 +39,19 @@ void report();
 void attemptSensorReadAndReport();
 void maybeReconnect();
 
-void readConfiguration() {
-  StaticJsonBuffer<200> buffer;
-  char readFileBuffer[200];
-  if (!SPIFFS.begin()) {
-    Serial.println("SPIFFS could not be accessed");
-    return;
-  }
-
-  File f = SPIFFS.open("/config.json", "r");
-
-  if (!f) {
-    Serial.println("Opening config.json failed");
-    return;
-  }
-
-  Serial.println(f.readBytes(readFileBuffer, 200));
-  Serial.println(readFileBuffer);
-  JsonObject &root = buffer.parse(readFileBuffer);
-
-  if (!root.success()) {
-    Serial.println("Parsing config.json failed");
-    return;
-  }
-
-  WIFI_SSID = root["wifi_ssid"].asString();
-  WIFI_PASS = root["wifi_pass"].asString();
-  MQTT_SERVER = root["mqtt_server"].asString();
-  MQTT_PORT = root["mqtt_port"].as<int>();
-  METER_PULSE_WIDTH = root["meter_pulse_width"].as<int>();
-
-  SPIFFS.end();
-}
-
 void setup() {
   Serial.begin(115200);
   Serial.println();
+  Serial.println();
+  Serial.println("ESP8266 Power Monitor");
   Serial.print("Built on ");
   Serial.print(__DATE__);
   Serial.print(" ");
-  Serial.println(__TIME__);
-  Serial.print("Git revision: ");
+  Serial.print(__TIME__);
+  Serial.print(" from ");
   Serial.println(GIT_REVISION);
-  Serial.println("Starting up.");
-
-  readConfiguration();
+  Serial.println("-----------------------");
+  Serial.flush();
 
   // Set up sensors, interrupts, etc.
   // When there's a pulse, increment the counter.
@@ -107,25 +63,15 @@ void setup() {
   attachInterrupt(PULSE_PIN, pulseStart, FALLING);
   attachInterrupt(PIR_PIN, recordMovement, RISING);
 
-  WiFi.setOutputPower(20.5); // Set output power to max, see if it helps.
-  WiFiMulti.addAP(WIFI_SSID.c_str(), WIFI_PASS.c_str());
-  mqtt.setServer(MQTT_SERVER.c_str(), MQTT_PORT);
-}
-
-void error(int num) {
-  int i = 0;
-  digitalWrite(LED_PIN, HIGH);
-  for (i = 0; i < num; i++) {
-    digitalWrite(LED_PIN, LOW);
-    delay(100);
-    digitalWrite(LED_PIN, HIGH);
-    delay(100);
-  }
+  network::start("ESP8266-PowerMonitor");
+  network::hello();
+  updater::begin();
 }
 
 void loop() {
   int cycles = REPORT_FREQUENCY;
   while (cycles > 0) {
+    network::loop();
     Serial.print("Measuring. Seconds left: ");
     Serial.println(cycles--);
     if (cycles % 30 == 0) {
@@ -134,8 +80,7 @@ void loop() {
     }
     delay(1000);
   }
-  maybeReconnect();
-  determineNodeName();
+
   attemptSensorReadAndReport();
   ESP.restart();
 }
@@ -161,66 +106,6 @@ void pulseEnd() {
   attachInterrupt(PULSE_PIN, pulseStart, FALLING);
 }
 
-void report() {
-  maybeReconnect();
-  Serial.println(millis());
-  Serial.println("Sending data");
-
-  voltage = ESP.getVcc() / 1000.0;
-  rssi = WiFi.RSSI();
-  temperature = sensors.getTempCByIndex(0);
-
-  String stream;
-  StaticJsonBuffer<200> buffer;
-  JsonObject& root = buffer.createObject();
-  root["temperature"] = temperature;
-  root["movementCounter"] = movementCounter;
-  root["energyCounter"] = energyCounter;
-  root["errorCounter"] = errorCounter;
-  root["voltage"] = voltage;
-  root["rssi"] = rssi;
-  root["packetsSent"] = packetsSent;
-  root["uptime"] = millis();
-  root.printTo(stream);
-  Serial.println(stream);
-
-  String clientName = "ESP8266-PowerMonitor " + nodeName;
-  
-  if (mqtt.connect(clientName.c_str())) {
-    String topic = "/devices/" + nodeName;
-    Serial.println("Publishing to " + topic);
-    if (mqtt.publish(topic.c_str(), stream.c_str(), true)) {
-      Serial.println("Published successfully.");
-    }
-    mqtt.disconnect();
-  }
-}
-
-void maybeReconnect() {
-  WiFi.reconnect();
-  while (WiFiMulti.run() != WL_CONNECTED) {
-    Serial.print("(Re)connecting to: ");
-    Serial.println(WIFI_SSID);
-    delay(1000);
-  }
-  Serial.print("connected to: ");
-  Serial.println(WiFi.BSSIDstr());
-  Serial.print("IP: ");
-  Serial.println(WiFi.localIP());
-}
-
-void determineNodeName() {
-  nodeName = WiFi.macAddress();
-  for (int i = nodeName.indexOf(':'); i > -1; i = nodeName.indexOf(':')) nodeName.remove(i, 1);
-  nodeName.toLowerCase();
-
-  Serial.print("Node name: ");
-  Serial.println(nodeName);
-
-  MDNS.begin(nodeName.c_str());
-  MDNS.addService("http", "tcp", 80);
-}
-
 void attemptSensorReadAndReport() {
   sensors.begin();
   int tries = 0;
@@ -237,9 +122,13 @@ void attemptSensorReadAndReport() {
   if (success) {
     sensors.setResolution(12);
     sensors.requestTemperatures();
-    report();
+      temperature = sensors.getTempCByIndex(0);
+    network::report(temperature, NAN, NAN, ESP.getVcc() / 1000);
   } else {
     errorCounter++;
-    error(tries);
   }
+}
+
+void messageReceived(String topic, String payload, char * bytes, unsigned int length) {
+  network::mqtt_message_received_cb(topic, payload, bytes, length);
 }
